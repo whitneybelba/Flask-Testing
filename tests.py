2e2e5be334f1d7a9331f63fd9a9d33aabf1b10db
Hackbright Student <info@hackbrightacademy.com>
2016-08-05 13:02:18 -0700

    Initial commit of Flask testing exercise.

---
+++ tests.py
@@ -0,0 +1,66 @@
+import unittest
+
+from party import app
+from model import db, example_data, connect_to_db
+
+
+
+class PartyTests(unittest.TestCase):
+    """Tests for my party site."""
+
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+
+    def test_homepage(self):
+        result = self.client.get("/")
+        self.assertIn("board games, rainbows, and ice cream sundaes", result.data)
+
+
+    def test_no_rsvp_yet(self):
+        # FIXME: Add a test to show we see the RSVP form, but NOT the
+        # party details
+        result = self.client.get("/")
+        self.assertNotIn("Party Details", result.data)
+        
+
+
+    def test_rsvp(self):
+        result = self.client.post("/rsvp",
+                                  data={"name": "Jane",
+                                        "email": "jane@jane.com"},
+                                  follow_redirects=True)
+        self.assertIn("Party Details", result.data)
+
+
+class PartyTestsDatabase(unittest.TestCase):
+    """Flask tests that use the database."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+        # Connect to test database (uncomment when testing database)
+        # connect_to_db(app, "postgresql:///testdb")
+
+        # Create tables and add sample data (uncomment when testing database)
+        # db.create_all()
+        # example_data()
+
+    def tearDown(self):
+        """Do at end of every test."""
+
+        # (uncomment when testing database)
+        # db.session.close()
+        # db.drop_all()
+
+    def test_games(self):
+        #FIXME: test that the games page displays the game from example_data()
+        print "FIXME"
+
+
+if __name__ == "__main__":
+    unittest.main()
